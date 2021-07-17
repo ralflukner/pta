f7426cda0db6086ecd5d3a7683dc8077612f82e5
Ralf B. Lukner MD PhD <lukner@gmail.com>
2021-07-17 15:50:54 -0500

    Added Login PHP

---
+++ login.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+
+<head>
+    <base href="/">
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/4/css/bootstrap.min.css" />
+</head>
+
+<body is="dmx-app" id="login">
+    <header>
+        <h1>User Login</h1>
+        <main>
+            <form id="form1">
+                <div class="form-row">
+                    <div class="col">
+                        <textarea id="text_user_name_label" class="form-control" name="UserNameLabel" value="User Name"></textarea>
+                    </div>
+                </div>
+            </form>
+        </main>
+        <footer></footer>
+    </header>
+    <script src="bootstrap/4/js/popper.min.js"></script>
+    <script src="bootstrap/4/js/bootstrap.min.js"></script>
+</body>
+
+</html>
