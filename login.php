a79827c6fd7a13627f9027836cf7308da9da491b
Ralf B. Lukner MD PhD <lukner@gmail.com>
2021-07-18 19:06:08 -0500

    Attempting to isolate the 404 Error after submit

--- login.php
+++ login.php
@@ -25,18 +25,33 @@ echo "Current Date and Time = "
 
 // Upload a file
 echo <<<_END
-            <html lang="en-us"><head><title>PHP Form Upload</title></head><body>
-            <form method='post' action='login.php' enctype='multipart/form-data'>
-            Select File: <input type='file' name='filename' size='10'>
-            <input type='submit' value='Upload'>
-            </form>
-        _END;
+ <form action="login.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+ </form>
+_END;
 
-if ($_FILES) {
-    echo "filename = " . $_FILES['filename']['name'];
-    $name = $_FILES['filename']['name'];
-    move_uploaded_file($_FILES['filename']['tmp_name'], $name);
+if (1==1) {
 
+    $target_dir = "tmp/";
+    echo $_FILES;
+    // print_r($_FILES);
+    //$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    // echo 'The target file name with path is ' . $target_dir . $target_file;
+    // $uploadOk = 1;
+    /* $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    } */
 }
 ?>
 
